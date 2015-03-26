0db7a2e93be0feda18369acc383b313933f74a5c
Brandon VanderMey <brandon.vandermey@gmail.com>
2015-03-26 16:24:44 -0400

    Timestamp

--- src/Main_CI.java
+++ src/Main_CI.java
@@ -1,5 +1,7 @@
 //	Main_CI.java by Kyle Wolff and Brandon VanderMey on 3/25/2015
 
+import java.util.Date;
+
 import javax.swing.*;
 
 public class Main_CI
@@ -12,6 +14,9 @@ public class Main_CI
 		
 		JOptionPane.showMessageDialog(null, "\nVehicle descriptions:\n" + aBoat.toString() + "\n" + aBike.toString());
 		*/
+		
+		Date date = new Date();
+		System.out.println("Car Insurance by Kyle Wolff and Brandon VanderMey on " + date);
 		
 		InsuredCar myCar = new InsuredCar();
 		JOptionPane.showMessageDialog(null, myCar.toString());
